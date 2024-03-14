4194f184b22f778ae6ac2f621a91567390aec855
Cherry291 <71474660+Cherry291@users.noreply.github.com>
2024-03-14 10:33:00 +0800

    fix Google AD

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function App() {
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5517951218257829"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           ></script>
         </>
       )}
