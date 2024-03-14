e01cddf46360b84b9034c680c2c796cab8c0a25c
Cherry291 <71474660+Cherry291@users.noreply.github.com>
2024-03-14 10:30:23 +0800

    add Google AD

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,15 @@ export default async function App() {
       {serverConfig?.isVercel && (
         <>
           <Analytics />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5517951218257829"
+            crossorigin="anonymous"
+          ></script>
         </>
       )}
     </>
   );
 }
+
+
