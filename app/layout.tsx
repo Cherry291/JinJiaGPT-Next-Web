60a1f977fc2f0e60195823bfa2ef0dc092260993
数字游牧人 <71474660+flyweb291@users.noreply.github.com>
2024-04-13 20:36:25 +0800

    google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5517951218257829"
+          crossorigin="anonymous"></script>
       </head>
       <body>
         {children}
